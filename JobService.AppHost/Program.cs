e340f1a22229b8abd95f45078b1dd1625f874e07
Arno Noordover <anoordover@users.noreply.github.com>
2024-05-24 00:01:41 +0200

    Added PostGreSQL and initial migration

--- JobService.AppHost/Program.cs
+++ JobService.AppHost/Program.cs
@@ -2,5 +2,14 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<JobService_Service>("JobService");
+var postgres = builder.AddPostgres("postgres",
+        builder.CreateResourceBuilder<ParameterResource>(new ParameterResource("username",
+            _ => "postgres")),
+        builder.CreateResourceBuilder<ParameterResource>(new ParameterResource("password",
+            _ => "Password12!")),
+        5432)
+    .AddDatabase("JobService", "JobService");
+
+builder.AddProject<JobService_Service>("JobServiceService")
+    .WithReference(postgres);
 builder.Build().Run();
--- JobService.Service/Program.cs
+++ JobService.Service/Program.cs
@@ -1,7 +1,20 @@
+using System.Reflection;
+
+using JobService.Service;
+
+using MassTransit.EntityFrameworkCoreIntegration;
+
+using Microsoft.EntityFrameworkCore;
+
 using Serilog;
 using Serilog.Events;
 
-Log.Logger = new LoggerConfiguration()
+var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
+
+var optionsEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+var useOtlpExporter = !string.IsNullOrWhiteSpace(optionsEndpoint);
+var logBuilder = new LoggerConfiguration()
     .MinimumLevel.Information()
     .MinimumLevel.Override("MassTransit", LogEventLevel.Debug)
     .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
@@ -9,14 +22,76 @@ Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
     .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
     .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .CreateLogger();
+    .WriteTo.Console();
+if (useOtlpExporter)
+{
+    logBuilder.WriteTo.OpenTelemetry(options =>
+    {
+        options.Endpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]; 
+        AddHeaders(options.Headers, builder.Configuration["OTEL_EXPORTER_OTLP_HEADERS"]);
+        AddResourceAttributes(options.ResourceAttributes, builder.Configuration["OTEL_RESOURCE_ATTRIBUTES"]);
 
+        options.ResourceAttributes.Add("service.name", "apiservice");
+        return;
 
-var builder = WebApplication.CreateBuilder(args);
+        void AddResourceAttributes(IDictionary<string, object> attributes, string attributeConfig)
+        {
+            if (!string.IsNullOrEmpty(attributeConfig))
+            {
+                var parts = attributeConfig.Split('=');
+                if (parts.Length == 2)
+                {
+                    attributes[parts[0]] = parts[1];
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Invalid resource attribute format: {attributeConfig}");
+                }
+            }
+        }
+
+        void AddHeaders(IDictionary<string, string> headers, string headerConfig)
+        {
+            if (!string.IsNullOrEmpty(headerConfig))
+            {
+                foreach (var header in headerConfig.Split(','))
+                {
+                    var parts = header.Split('=');
+                    if (parts.Length == 2)
+                    {
+                        headers[parts[0]] = parts[1];
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException($"Invalid header format: {header}");
+                    }
+                }
+            }
+        }
+    });
+}
+
+Log.Logger = logBuilder.CreateBootstrapLogger();
+builder.Logging.ClearProviders().AddSerilog();
+builder.Host.UseSerilog();
+Log.Logger.Information("Configured Serilog to send to: {Endpoint}", optionsEndpoint);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<JobServiceSagaDbContext>(optionsBuilder =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("JobService");
+    Log.Logger.Information("ConnectionString: {ConnectionString}", connectionString);
+
+    optionsBuilder.UseNpgsql(connectionString, m =>
+    {
+        m.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+        m.MigrationsHistoryTable($"__{nameof(JobServiceSagaDbContext)}");
+    });
+});
+builder.Services.AddHostedService<MigrationHostedService<JobServiceSagaDbContext>>();
 
 var app = builder.Build();
 
@@ -29,5 +104,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.Run();
+await app.RunAsync();
 
